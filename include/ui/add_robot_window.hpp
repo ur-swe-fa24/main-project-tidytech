9e304acecf14b80326a8d55e37a48f62ce88607b
squireaa <squireaa1@gmail.com>
2024-12-04 18:07:30 -0500

    Fixed error

--- include/ui/add_robot_window.hpp
+++ include/ui/add_robot_window.hpp
@@ -33,7 +33,7 @@ class AddRobotWindow : public wxDialog {
         std::string get_charging_position() {
             int sol;
             for (int i = 0; i < 11; i++) {
-                if (boxes_[i] == NULL) {
+                if (boxes_[i] == nullptr) {
                     break;
                 } else if (boxes_[i]->GetValue()) {
                     sol = i + 1;
@@ -55,17 +55,17 @@ class AddRobotWindow : public wxDialog {
         wxTextCtrl* current_position_;
         wxTextCtrl* charging_position_;
         wxTextCtrl* robot_name_;
-        wxRadioButton* floor1;
-        wxRadioButton* floor2;
-        wxRadioButton* floor3;
-        wxRadioButton* floor4;
-        wxRadioButton* floor5;
-        wxRadioButton* floor6;
-        wxRadioButton* floor7;
-        wxRadioButton* floor8;
-        wxRadioButton* floor9;
-        wxRadioButton* floor10;
-        wxRadioButton* floor11;
+        wxRadioButton* floor1 = nullptr;
+        wxRadioButton* floor2 = nullptr;
+        wxRadioButton* floor3 = nullptr;
+        wxRadioButton* floor4 = nullptr;
+        wxRadioButton* floor5 = nullptr;
+        wxRadioButton* floor6 = nullptr;
+        wxRadioButton* floor7 = nullptr;
+        wxRadioButton* floor8 = nullptr;
+        wxRadioButton* floor9 = nullptr;
+        wxRadioButton* floor10 = nullptr;
+        wxRadioButton* floor11 = nullptr;
         int num_added_;
         std::vector<wxRadioButton*> boxes_ = {floor1, floor2, floor3, floor4, floor5, floor6, floor7, floor8, floor9, floor10, floor11};
 
